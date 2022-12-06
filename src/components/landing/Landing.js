ef1025b3bc32f5770bcb268b959947d7f97041b5
giovani94 <giovaniportal@gmail.com>
2022-12-05 21:05:13 -0800

    Added Navigation and Hero

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "../main/Main";
+import Navigation from "../navigation/Navigation";
+
+const Landing = () => {
+	return (
+		<>
+			<Navigation />\
+			<Main />
+		</>
+	);
+};
+
+export default Landing;
